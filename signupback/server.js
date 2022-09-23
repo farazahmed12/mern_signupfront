2a1123be452d2c8755b92cc54d46f31777f9ae50
Faraz Ahmed <masterdot707@gmail.com>
2022-09-24 04:01:03 +0500

    connected to mongoDB

--- signupback/server.js
+++ signupback/server.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose.connect(process.env.DATABASE_ACCESS, () =>
+  console.log("DB connected")
+);
 
 app.listen(4000, () => console.log("Server up and running"));
