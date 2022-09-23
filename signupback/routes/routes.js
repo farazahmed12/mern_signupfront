a14da3265c03b2e5a282caecd9750a4f1d70e8cb
Faraz Ahmed <masterdot707@gmail.com>
2022-09-24 04:29:48 +0500

    done with server side.

--- signupback/routes/routes.js
+++ signupback/routes/routes.js
@@ -9,12 +9,15 @@ router.post("/signup", (request, response) => {
     username: request.body.username,
     email: request.body.email,
     password: request.body.password,
-  })
-  signedUpUser.save()
-  .then((data) => {
-      response.json(data)
-}).catch( (error) => {
-  response.json(error)
-})
+  });
+  signedUpUser
+    .save()
+    .then((data) => {
+      response.json(data);
+    })
+    .catch((error) => {
+      response.json(error);
+    });
+});
 
 module.exports = router;
--- signupback/server.js
+++ signupback/server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
+
+const routesUrls = require("./routes/routes.js");
 
 dotenv.config();
+
 mongoose.connect(process.env.DATABASE_ACCESS, () =>
   console.log("DB connected")
 );
 
+app.use(express.json());
+app.use(cors());
+app.use("/app", routesUrls);
 app.listen(4000, () => console.log("Server up and running"));
