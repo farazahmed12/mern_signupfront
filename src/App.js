9e68581b368cf1255b2ae334d3cb920f7cefae5f
Faraz Ahmed <masterdot707@gmail.com>
2022-09-26 15:35:08 +0500

    Created Frontend UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
+  const [state, setState] = React.useState({
+    fullName: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+  const changeFullname = (e) => {
+    setState({
+      ...state,
+      fullName: e,
+    });
+  };
+  const changeUsername = (e) => {
+    setState({
+      ...state,
+      username: e,
+    });
+  };
+  const changeEmail = (e) => {
+    setState({
+      ...state,
+      email: e,
+    });
+  };
+  const changePassword = (e) => {
+    setState({
+      ...state,
+      password: e,
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Sign up save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="form-div">
+        <form>
+          <input
+            placeholder="Full Name"
+            type="text"
+            onChange={(e) => changeFullname(e.target.value)}
+            value={state.fullName}
+            className="form-control form-group"
+          />
+          <input
+            placeholder="Username"
+            type="text"
+            onChange={(e) => changeUsername(e.target.value)}
+            value={state.username}
+            className="form-control form-group"
+          />
+          <input
+            placeholder="E-mail"
+            type="text"
+            onChange={(e) => changeEmail(e.target.value)}
+            value={state.email}
+            className="form-control form-group"
+          />
+          <input
+            placeholder="Password"
+            type="text"
+            onChange={(e) => changePassword(e.target.value)}
+            value={state.password}
+            className="form-control form-group"
+          />
+
+          <input
+            className="btn btn-danger btn-block"
+            type="submit"
+            value="Submit"
+          />
+        </form>
+      </div>
     </div>
   );
 }
